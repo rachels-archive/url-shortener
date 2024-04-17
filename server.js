d13db0008e30116def5a988ff27726515da4dadd
rachels-archive <rachelhui@protonmail.com>
2024-04-17 10:27:22 +0800

    Add delete method for urls

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override');
 const ShortUrl = require('./models/shortUrls')
 const app = express()
 
@@ -12,6 +13,18 @@ parse the body of POST requests so that req.body.fullUrl
 can be accessed in the /shortUrls route handler
 */
 app.use(express.urlencoded({extended: false}))
+
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // Extract the HTTP method from the _method field
+        var method = req.body._method;
+        console.log(method, req.body._method);
+        // Delete the _method field from the request body
+        delete req.body._method;
+        // Return the extracted method, which will be used as the overridden HTTP method
+        return method;
+    }
+}));
 
 app.get('/', async (req, res) => {
     const shortUrls = await ShortUrl.find()
@@ -35,6 +48,12 @@ app.get('/:shortUrl', async (req, res) => {
     shortUrl.clicks ++
     await shortUrl.save()
     res.redirect(shortUrl.full)
+})
+
+app.delete(('/shortUrl/:id'), async (req, res) => {
+    const url = await ShortUrl.findByIdAndDelete(req.params.id);
+    if (url == null) return res.sendStatus(404)
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT || 3000);
